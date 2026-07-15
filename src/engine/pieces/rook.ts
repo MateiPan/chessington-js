f0963102a388bc87c14556072da1c7a35c071717
Matei P <Matei.Panzariu@softwire.com>
2026-07-15 17:01:03 +0300

    Added functionality for not allowing the rook to pass other pieces

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -11,17 +11,54 @@ export default class Rook extends Piece {
     public getAvailableMoves(board: Board): Square[] {
         let rookPosition: Square = board.findPiece(this);
 
+        let row: number = rookPosition.row;
+        let col: number = rookPosition.col;
         let moves: Square[] = [];
 
-        // Adds the possible moves that can be made on a certain row
-        for (let i: number = 0; i < 8; i++)
-            if(rookPosition.col !== i)
-                moves.push(Square.at(rookPosition.row, i));
+        // the rook can only move from the left boundary to the right boundary
+        // and from the bottom boundary to the top boundary
+        let leftBoundary: number = -1;
+        let rightBoundary: number = 8;
+        let topBoundary: number = 8;
+        let bottomBoundary: number = -1;
 
-        // Adds the possible moves that can be made on a certain column
-        for (let j: number = 0; j < 8; j++)
-            if(rookPosition.row !== j)
-                moves.push(Square.at(j, rookPosition.col));
+        // we check the places below the rook, to see if there are any pieces there
+        for (let i: number = row - 1; i >= 0; i--)
+            if (board.getPiece(Square.at(i, col)) !== undefined) {
+                bottomBoundary = i;
+                break;
+            }
+
+        // we check the places above the rook, to see if there are any pieces there
+        for (let i: number = row + 1; i < 8; i++)
+            if (board.getPiece(Square.at(i, col)) !== undefined) {
+                topBoundary = i;
+                break;
+            }
+
+        // we check the places left of the rook, to see if there are any pieces there
+        for (let j: number = col - 1; j >= 0; j--)
+            if (board.getPiece(Square.at(row, j)) !== undefined) {
+                leftBoundary = j;
+                break;
+            }
+
+        // we check the places right of the rook, to see if there are any pieces there
+        for (let j: number = col + 1; j < 8; j++)
+            if (board.getPiece(Square.at(row, j)) !== undefined) {
+                rightBoundary = j;
+                break;
+            }
+
+        // Iterate and add all the squares between the left and right boundary
+        for (let j: number = leftBoundary + 1; j < rightBoundary; j++)
+            if(rookPosition.col !== j)
+                moves.push(Square.at(rookPosition.row, j));
+
+        // Iterate and add all the squares between the top and bottom boundary
+        for (let i: number = bottomBoundary + 1; i < topBoundary; i++)
+            if(rookPosition.row !== i)
+                moves.push(Square.at(i, rookPosition.col));
 
         return moves;
     }
